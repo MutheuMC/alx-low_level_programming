6eafc2c4c33adcc50b83075e029a16a935f5ea0f
MutheuMC <cheldeanmutheu1@ggmail.com>
2022-09-30 02:33:51 -0700

    changes

---
+++ 1-args.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main- prints the number of arguments
+ * has two arguments argc and argv
+ * @argc:print sthe count of the arguments in the
+ * command line
+ * @argv: argument vector
+ * description- print the number of arguments
+ * Return: always 0 success
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
